3b58977c1bdeaf0abf1cd8248cc007acacd4f766
Charles Dobson <79115124+DobbaWon@users.noreply.github.com>
2025-02-24 13:44:34 +0000

    Add files via upload

---
+++ Generate.java
@@ -0,0 +1,8 @@
+public class Generate extends AbstractGenerate {
+
+    @Override
+    public void reportError(Token token, String explanatoryMessage) throws CompilationException {
+
+    }
+    
+}
---
+++ SyntaxAnalyser.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class SyntaxAnalyser extends AbstractSyntaxAnalyser{
+    public SyntaxAnalyser(String text){
+
+    }
+    
+    @Override
+    public void _statementPart_() throws IOException, CompilationException {
+        
+    }
+
+    @Override
+    public void acceptTerminal(int symbol) throws IOException, CompilationException {
+        
+    }
+}
